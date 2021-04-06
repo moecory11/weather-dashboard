fd9067ff2e1560d810a327dfdadf924506219fd5
Cory Moe <moecory11@gamil.com>
2021-04-05 21:56:07 -0700

    updated, css and readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # weather-dashboard
+## Description
+For this project my motivation was to create a weather dashboard that gives you the forecast for the upcoming week and current weather. 
+## Installation
+To access the HTML, CSS and JS you can go to the GitHub repository for this project:
+https://github.com/moecory11/work-day-scheduler
+## Usage
+When using this weather dashboard all you need to do is enter the city for which you want to know the weather conditions and press the search button. The search will return the current date, temerature, humidity and wind speed of the city you selected. In addition it will provide a 5 day forecast. After you enter a new city to search, your previous city search will be saved in the list below.
+![Website with navigation bar](https://github.com/moecory11/work-day-scheduler/blob/main/images/Day%20Planner%20Screenshot.png?raw=true)
+
+## Credits
+While creating this weather dashboard my main source of references and resources came from the examples and activities in my daily class content as well as mini projects.
+## License
+MIT License
+
+Copyright (c) [2021] [CoryMoe]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+---
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,153 @@
-var responseText = document.getElementById('root');
-requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=seattle&appid=7cfd08227ecf263cbffd0eac74bb8f69&units=imperial'
-function getApi(requestUrl) {
-  fetch(requestUrl)
-    .then(function (response) {
-      if (response.status === 200) {
-        responseText.textContent = response.status;
-      }
-      return response.json();
-  });
-}
-getApi(requestUrl);
-
+var history = JSON.parse(localStorage.getItem("weatherInfo")) || [];
 var searchForm = document.querySelector("#search-form");
-var serchTermInput = document.querySelector("#search-bar")
+var serchTermInput = document.querySelector("#search-bar");
 
-searchForm.addEventListener("submit", function(event){
+searchForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log("form submitted!")
   var searchTerm = serchTermInput.value;
-  console.log(searchTerm);
-  var urlToFetch = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=7cfd08227ecf263cbffd0eac74bb8f69&units=imperial`
-  fetch(urlToFetch).then(function(response){
+  //history.push(searchTerm);
+  localStorage.setItem("weatherInfo", JSON.stringify(history))
+  listItem(searchTerm)
+  var urlToFetch = `https://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&appid=7cfd08227ecf263cbffd0eac74bb8f69&units=imperial`
+
+  fetch(urlToFetch).then(function (response) {
     return response.json()
-  }).then(function(data){
-    console.log(data.name);
-    console.log(data.dt);
-    console.log(data.weather[0].icon);
-    console.log(data.main.temp);
-    console.log(data.main.humidity);
-    console.log(data.wind.speed);
+  }).then(function (data) {
+    var resultContent = document.getElementById('root');
+    resultContent.innerHTML = ""
+    var cityh2 = document.createElement("h2");
+    cityh2.textContent = "City: " + data.city.name;
+    resultContent.append(cityh2);
+
+    var dateh3 = document.createElement("h3");
+    dateh3.textContent = new Date().toDateString();
+    resultContent.append(dateh3);
+
+    var iconh3 = document.createElement("img");
+    iconh3.setAttribute("src", "http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + ".png");
+    resultContent.append(iconh3);
+
+    var temph3 = document.createElement("h3");
+    temph3.textContent = "Temperature: " + data.list[0].main.temp;
+    resultContent.append(temph3);
+
+    var humidityh3 = document.createElement("h3");
+    humidityh3.textContent = "Humidity: " + data.list[0].main.humidity;
+    resultContent.append(humidityh3);
+
+    var windspeedh3 = document.createElement("h3");
+    windspeedh3.textContent = "Wind Speed: " + data.list[0].wind.speed;
+    resultContent.append(windspeedh3);
+
+    var day1dateh3 = document.createElement("h3");
+    day1dateh3.textContent = data.list[7].dt_txt.split(" ")[0];
+    var day1resultContent = document.getElementById("card-1");
+    day1resultContent.innerHTML = ""
+    day1resultContent.append(day1dateh3);
+
+    var day1iconh3 = document.createElement("img");
+    day1iconh3.setAttribute("src", "http://openweathermap.org/img/wn/" + data.list[7].weather[0].icon + ".png");
+    var day1resultContent = document.getElementById("card-1");
+    day1resultContent.append(day1iconh3);
+
+    var day1temph3 = document.createElement("h3");
+    day1temph3.textContent = "Temp: " + data.list[7].main.temp;
+    var day1resultContent = document.getElementById("card-1");
+    day1resultContent.append(day1temph3);
+
+    var day1humidityh3 = document.createElement("h3");
+    day1humidityh3.textContent = data.list[7].main.humidity;
+    var day1resultContent = document.getElementById("card-1");
+    day1resultContent.append("Humidity: ", day1humidityh3);
+
+    var day2dateh3 = document.createElement("h3");
+    day2dateh3.textContent = data.list[15].dt_txt.split(" ")[0];
+    var day2resultContent = document.getElementById("card-2");
+    day2resultContent.innerHTML = ""
+    day2resultContent.append(day2dateh3);
+
+    var day2iconh3 = document.createElement("img");
+    day2iconh3.setAttribute("src", "http://openweathermap.org/img/wn/" + data.list[15].weather[0].icon + ".png");
+    var day2resultContent = document.getElementById("card-2");
+    day2resultContent.append(day2iconh3);
+
+    var day2temph3 = document.createElement("h3");
+    day2temph3.textContent = "Temp: " + data.list[15].main.temp;
+    var day2resultContent = document.getElementById("card-2");
+    day2resultContent.append(day2temph3);
+
+    var day2humidityh3 = document.createElement("h3");
+    day2humidityh3.textContent = data.list[15].main.humidity;
+    var day2resultContent = document.getElementById("card-2");
+    day2resultContent.append("Humidity: ", day2humidityh3);
+
+    var day3dateh3 = document.createElement("h3");
+    day3dateh3.textContent = data.list[23].dt_txt.split(" ")[0];
+    var day3resultContent = document.getElementById("card-3");
+    day3resultContent.innerHTML = ""
+    day3resultContent.append(day3dateh3);
+
+    var day3iconh3 = document.createElement("img");
+    day3iconh3.setAttribute("src", "http://openweathermap.org/img/wn/" + data.list[23].weather[0].icon + ".png");
+    var day3resultContent = document.getElementById("card-3");
+    day3resultContent.append(day3iconh3);
+
+    var day3temph3 = document.createElement("h3");
+    day3temph3.textContent = "Temp: " + data.list[23].main.temp;
+    var day3resultContent = document.getElementById("card-3");
+    day3resultContent.append(day3temph3);
+
+    var day3humidityh3 = document.createElement("h3");
+    day3humidityh3.textContent = data.list[23].main.humidity;
+    var day3resultContent = document.getElementById("card-3");
+    day3resultContent.append("Humidity: ", day3humidityh3);
+
+    var day4dateh3 = document.createElement("h3");
+    day4dateh3.textContent = data.list[31].dt_txt.split(" ")[0];
+    var day4resultContent = document.getElementById("card-4");
+    day4resultContent.innerHTML = ""
+    day4resultContent.append(day4dateh3);
+
+    var day4iconh3 = document.createElement("img");
+    day4iconh3.setAttribute("src", "http://openweathermap.org/img/wn/" + data.list[31].weather[0].icon + ".png");
+    var day4resultContent = document.getElementById("card-4");
+    day4resultContent.append(day4iconh3);
+
+    var day4temph3 = document.createElement("h3");
+    day4temph3.textContent = "Temp: " + data.list[31].main.temp;
+    var day4resultContent = document.getElementById("card-4");
+    day4resultContent.append(day4temph3);
+
+    var day4humidityh3 = document.createElement("h3");
+    day4humidityh3.textContent = + data.list[31].main.humidity;
+    var day4resultContent = document.getElementById("card-4");
+    day4resultContent.append("Humidity: ", day4humidityh3);
+
+    var day5dateh3 = document.createElement("h3");
+    day5dateh3.textContent = data.list[39].dt_txt.split(" ")[0];
+    var day5resultContent = document.getElementById("card-5");
+    day5resultContent.innerHTML = ""
+    day5resultContent.append(day5dateh3);
+
+    var day5iconh3 = document.createElement("img");
+    day5iconh3.setAttribute("src", "http://openweathermap.org/img/wn/" + data.list[39].weather[0].icon + ".png");
+    var day5resultContent = document.getElementById("card-5");
+    day5resultContent.append(day5iconh3);
+
+    var day5temph3 = document.createElement("h3");
+    day5temph3.textContent = "Temp: " + data.list[39].main.temp;
+    var day5resultContent = document.getElementById("card-5");
+    day5resultContent.append(day5temph3);
+
+    var day5humidityh3 = document.createElement("h3");
+    day5humidityh3.textContent = data.list[39].main.humidity;
+    var day5resultContent = document.getElementById("card-5");
+    day5resultContent.append("Humidity: ", day5humidityh3);
   })
 })
-
-
-
+function listItem(cityname) {
+  var li = document.createElement("li")
+  li.setAttribute("class", "list-group-item")
+  li.textContent = cityname
+  document.getElementById("search-history").append(li)
+}
